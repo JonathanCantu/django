3430b7a7f487e401c9fbd1f6aa49b1631ad210cd
JonathanCantu <jcantu@jcantu.com>
2025-02-04 08:51:17 -0700

    Refactoring and cleanup

--- elements/views.py
+++ elements/views.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from elements.models import Elements
 
 def index(request):
-    begin_table = "<table border='2'>"
-
-    caption = "<caption>Periodic Table of the Elements</caption>"
-
-    header = "<tr><th>AtomicNumber</th><th>Element</th><th>Symbol</th><th>AtomicMass</th></tr>"
+    template = "main/index.html"
 
     elements = Elements.objects.all()
+    context = {
+        'elements': elements
+    }
 
-    row = " "
-
-    for e in elements:
-        row = f"{row}<tr><td>{e.atomic_number}</td><td>{e.element}</td><td>{e.symbol}</td><td>{e.atomic_mass}</td></tr>"
-        
-    end_table = "</table>"
-
-    response = f"<html><body><img src='{% static 'images/atom-electrons.gif' %}' class='animated-atom' style='width:120px; margin-bottom:-50px;'>{begin_table}\n{caption}\n{header}\n{row}\n{end_table}\n</body></html>"
-
-    return HttpResponse(response)
+    return render(request, template, context)
 
---
+++ static/js/demo.js
@@ -0,0 +1,4 @@
+
+function submitClick() {
+    console.log("Inside Javascript method in demo.js.");
+}
